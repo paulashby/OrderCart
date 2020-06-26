84d280bf09398923ce58eb198dc3560b24d1cff6
Paul Ashby <paul@primitive.co>
2020-06-26 14:15:51 +0100

    Add TODO  - need field for Cart page url

--- OrderCart.config.php
+++ OrderCart.config.php
@@ -1,13 +1,6 @@
 <?php namespace ProcessWire;
 
-/**
- * Optional config file for PageMaker.module
- *
- * When present, the module will be configurable and the configurable properties
- * described here will be automatically populated to the module at runtime.  
- * 
- * For this module, this is populated after installation
- */
+//TODO: Add field for cart page url
 $config = array(
 	"outputCurrencySign" => array(
 		"name"=> "o_csign",
